015a3e75986259e14a7070bb1e01790aab667cde
Eric Mordezki <eric_mordezki@gmail.com>
2017-09-21 22:05:54 -0300

    Primer commit. Base de movimiento de auto con pista basica

---
+++ AnimalRace/Assets/Scripts/CarBehavior.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarBehavior : MonoBehaviour {
+    [SerializeField]
+    private float speed = 10;
+    [SerializeField]
+    private float rotationSpeed = 100;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        MoveForward();
+        TurnSideways();
+        //Jump();
+        //FireSpecialPower();
+    }
+
+    private void FireSpecialPower()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Jump()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void TurnSideways()
+    {
+        transform.Rotate(transform.up * rotationSpeed * Time.deltaTime * Input.GetAxis("Horizontal"));
+    }
+
+    private void MoveForward()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime * Input.GetAxis("Vertical");
+    }
+}
